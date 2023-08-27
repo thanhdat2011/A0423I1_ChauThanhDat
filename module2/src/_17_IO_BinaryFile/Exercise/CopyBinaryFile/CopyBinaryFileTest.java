1965d12f06f79f558f15a7e24ab3d3a77acfc787
thanhdat2011 <chauthanhdat2000@gmail.com>
2023-08-28 06:18:46 +0700

    u

--- module2/src/_17_IO_BinaryFile/Exercise/CopyBinaryFile/CopyBinaryFileTest.java
+++ module2/src/_17_IO_BinaryFile/Exercise/CopyBinaryFile/CopyBinaryFileTest.java
@@ -1,8 +1,36 @@
 package _17_IO_BinaryFile.Exercise.CopyBinaryFile;
 
-public class CopyBinaryFileTest {
-    private static String PATH = "";
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.List;
 
+public class CopyBinaryFileTest {
+
+    // data of product management exercise
+    private static final String SOURCE_PATH = "D:\\CodeGym\\module2\\src\\_17_IO_BinaryFile\\Exercise\\ProductManagement\\Data\\productData";
+    private static final String TARGET_PATH = "D:\\CodeGym\\module2\\src\\_17_IO_BinaryFile\\Exercise\\CopyBinaryFile\\copyBinaryFile";
+    public static void main(String[] args) {
+        try {
+            copyBinaryFile();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private static void copyBinaryFile() throws IOException {
+        FileInputStream fis = new FileInputStream(SOURCE_PATH);
+        FileOutputStream fos = new FileOutputStream(TARGET_PATH);
+
+        byte[] bytes = new byte[1024];
+
+        while ((fis.read(bytes)) > 0) {
+            fos.write(bytes);
+        }
+
+        fis.close();
+        fos.close();
+
+        System.out.println("DONE COPY");
+    }
+
 }

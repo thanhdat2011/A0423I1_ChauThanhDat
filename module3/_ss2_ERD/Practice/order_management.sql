113557e34e919c5f0b8dca8cdf1d9655447eb3d4
thanhdat2011 <chauthanhdat2000@gmail.com>
2023-10-02 00:39:31 +0700

    u

---
+++ module3/_ss2_ERD/Practice/order_management.sql
@@ -0,0 +1,11 @@
+create database orders_management;
+
+use orders_management;
+
+create table order_unit(
+	ou_code varchar(30),
+    ou_name varchar(30),
+    ou_address varchar(30),
+    ou_phone varchar(20),
+    primary key
+);
